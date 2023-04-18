d31f563aa30c7963e0af3d4ad7c86ab7e7e659f9
tantawy997 <abdelrhman.eltantawy97@gmail.com>
2023-04-18 03:30:26 +0200

    Add project files.

---
+++ DAL/ContextDB/AppDbContexts.cs
@@ -0,0 +1,29 @@
+﻿using DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.ContextDB
+{
+    public class AppDbContexts:DbContext
+    {
+        public AppDbContexts(DbContextOptions<AppDbContexts> options):base(options) 
+        { 
+
+        }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("server=.;database=MVCProjectRoute; trusted_connection=true;");
+        //}
+
+        public virtual DbSet<Department> Departments { get; set; }  
+
+        public virtual DbSet<Employee> Employees { get; set; }
+
+
+    }
+}
---
+++ PL/Helper/DocumentsSettings.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.IO;
+
+namespace PL.Helper
+{
+    public static class DocumentsSettings
+    {
+        public static string UploadFile(IFormFile file,string Source)
+        {
+            var folder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Files", Source);
+
+            var fileName = $"{Guid.NewGuid()}{Path.GetFileName(file.FileName)}";
+            var filePath = Path.Combine(folder, fileName);
+
+            using var fileStream = new FileStream(filePath, FileMode.Create);
+
+            file.CopyTo(fileStream);
+
+            return filePath;
+        }
+
+    }
+}
